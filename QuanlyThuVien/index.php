5d2de90ab1d8ac16c9662121dc6592c44c1a7337
DinhHongSon <32859078+DinhHongSon@users.noreply.github.com>
2017-10-17 14:50:44 +0700

    2

---
+++ QuanlyThuVien/index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang=''>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="public/css/styles.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+   <title>Quản Lý Tín Chỉr</title>
+</head>
+<body>
+
+<div id='cssmenu'>
+<ul>
+    <li class='active'><a href='Sinhvien.php'><span>Sinh Viên</span></a></li>
+    <li><a href='Monhoc.php'><span>Môn Học</span></a></li>
+   <li><a href='#'><span>Danh Sách Lớp Học</span></a></li>
+   <li class='last'><a href='#'><span>Contact</span></a></li>
+</ul>
+</div>
+
+</body>
+<html>
